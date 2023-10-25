c7a82089bf89b769318a56a90faf7ffd30abe5d8
ruki2117 <144656067+ruki2117@users.noreply.github.com>
2023-10-25 15:12:47 +0900

    Add files via upload

---
+++ a2_green/js/script.js
@@ -0,0 +1,34 @@
+$(function(){
+    $(".nav > ul > li").mouseover(function(){
+        $(".sub, .menubg").stop().slideDown();
+    });
+    $(".nav > ul > li").mouseouy(function(){
+        $(".sub, .menubg").stop().slideUp();
+    });
+    
+
+    let i = 0;
+    $(".slider ul").append($(".slider ul li").first().clone(true));
+    
+    setInterval(function(){
+        i++;
+        $(".slider ul").animate({marginLeft: -1200 * i +"px"}, 600);
+
+        if(i == 3){
+            setTimeout(function(){
+                $(".slider ul").animate({marginLeft: 0}, 0);
+                i=0;
+            }, 700);
+        }
+
+    }, 3000);
+
+    $(".pp").click(function(){
+        $(".popup").show();
+        return false;
+    });
+    $(".popup button").click(function(){
+        $(".popup").hide();
+        return false;
+    });
+})
